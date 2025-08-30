ce5bf90afa19b92e75f718ce7cc355a9204b9ae3
Erik Morén <erik.moren78@gmail.com>
2025-08-30 11:57:49 +0200

    setting expo-constants

--- app.config.js
+++ app.config.js
@@ -1,3 +1,6 @@
+import Constants from 'expo-constants';
+
+Constants.expoConfig.extra.OPENAI_API_KEY = process.env.EXPO_PUBLIC_OPENAI_API_KEY || ''
 
 module.exports = ({ config }) => {
     return {
