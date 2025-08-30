62d4e0e20f739744890dbb3fee99c97cf3712472
Erik Morén <erik.moren78@gmail.com>
2025-08-30 11:36:48 +0200

    Update type declarations, include EAS project ID and adjust app configuration

--- app.config.js
+++ app.config.js
@@ -1,15 +1,12 @@
 
 module.exports = ({ config }) => {
-
     return {
         ...config,
-        expo: {
-            name: "MeetingNotes",
-            slug: "MeetingNotes",
-            extra: {
-                ...config.expo.extra,
-                OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY
-            }
-        },
+        name: "MeetingNotes",
+        slug: "MeetingNotes",
+        extra: {
+            ...config.extra,
+            OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY || ''
+        }
     };
 };
